35db000d9f9ab8d231b599ec70fa49db2b23dd65
iwagami sho <cloudq9team2>
2014-06-13 01:20:04 +0900

    6_13_jQ

--- code4.js
+++ code4.js
@@ -12,7 +12,21 @@ function make(){
 	obj.returnname = function (){ return this.name; };
 	return obj;
 	}
+
+
+//domを使わない
+function handle( e) {
+	console.log( e);
+	}
 	
+	var div1 = document.getElementsByTagName( 'div')[0];
+	var div2 = document.getElementsByTagName( 'div')[1];
+	div1.addEventListener( 'click', handle, false);
+	div2.addEventListener( 'click', handle, false);
+	
+$('div').each( function() {
+		console.log( "he");
+	})
 	
 	var person = make();
 	person.getname("tanaka");
@@ -24,4 +38,17 @@ function make(){
 	printHash( hash);
 	printHash( array);
 	
-	
+$('div1').children().css({ margin: '50px'});
+$('div1').children().first().css({float:'left'});
+
+$(document).ready( function () {
+	$('div').mouseover( function () {
+		console.log("OVER!!");
+		$('div').click( function () {
+			console.log("click!!");
+		});
+		$('div').mouseout( function () {
+			console.log("OUT!!");
+		});
+	});
+});
